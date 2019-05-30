ec643a3eea51be7931632b8b2f4d7a82d2b7b485
Christopher Pappas <damassi.pappas@gmail.com>
2019-05-29 19:21:26 -0700

    Add basic failing example

--- src/components/__tests__/example.spec.js
+++ src/components/__tests__/example.spec.js
@@ -1,5 +1,22 @@
 /* eslint-disable */
+import React from 'react';
+import styled from 'styled-components/native';
+import renderer from 'react-test-renderer';
+// import { Theme, Box } from '@artsy/palette';
+
+const App = styled.View``;
+const Text = styled.Text`
+  color: black;
+`;
 
 test('example', () => {
-  expect(1 + 2).toBe(3);
+  const tree = renderer
+    .create(
+      <App>
+        <Text>hi</Text>
+      </App>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
